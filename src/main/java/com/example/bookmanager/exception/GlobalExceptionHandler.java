b6febde9dc650343ad485fdc57b2e2ef07e88b78
Tibor Palinkas <palinkastibor@gmail.com>
2026-08-14 22:38:08 +0200

    Add custom exception handling

--- src/main/java/com/example/bookmanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/bookmanager/exception/GlobalExceptionHandler.java
@@ -1,7 +1,39 @@
 package com.example.bookmanager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.example.bookmanager.exception.BusinessErrorCodes.*;
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<ExceptionResponse> handleException(LockedException e) {
+        return ResponseEntity
+                .status(UNAUTHORIZED)
+                .body(ExceptionResponse.builder()
+                        .businessErrorCode(ACCOUNT_LOCKED.getCode())
+                        .businessExceptionDescription(ACCOUNT_LOCKED.getDescription())
+                        .error(e.getMessage())
+                        .build()
+                );
+    }
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<ExceptionResponse> handleException(DisabledException e) {
+        return ResponseEntity
+                .status(UNAUTHORIZED)
+                .body(ExceptionResponse.builder()
+                        .businessErrorCode(ACCOUNT_DISABLED.getCode())
+                        .businessExceptionDescription(ACCOUNT_DISABLED.getDescription())
+                        .error(e.getMessage())
+                        .build()
+                );
+    }
 }
