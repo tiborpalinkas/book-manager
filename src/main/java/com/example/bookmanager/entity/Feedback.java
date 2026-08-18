d158b4c2cf4e3a9cbafeafeb2948deb9aa03995d
Tibor Palinkas <palinkastibor@gmail.com>
2026-08-18 20:06:22 +0200

    Add entity classes

---
+++ src/main/java/com/example/bookmanager/entity/Feedback.java
@@ -0,0 +1,20 @@
+package com.example.bookmanager.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    private Double note;
+    private String comment;
+}
--- src/main/java/com/example/bookmanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/bookmanager/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.example.bookmanager.exception;
 
-import org.springframework.http.HttpStatus;
+import jakarta.mail.MessagingException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.example.bookmanager.exception.BusinessErrorCodes.*;
 import static org.springframework.http.HttpStatus.*;
@@ -32,6 +37,56 @@ public class GlobalExceptionHandler {
                 .body(ExceptionResponse.builder()
                         .businessErrorCode(ACCOUNT_DISABLED.getCode())
                         .businessExceptionDescription(ACCOUNT_DISABLED.getDescription())
+                        .error(e.getMessage())
+                        .build()
+                );
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ExceptionResponse> handleException(BadCredentialsException e) {
+        return ResponseEntity
+                .status(UNAUTHORIZED)
+                .body(ExceptionResponse.builder()
+                        .businessErrorCode(BAD_CREDENTIALS.getCode())
+                        .businessExceptionDescription(BAD_CREDENTIALS.getDescription())
+                        .error(e.getMessage())
+                        .build()
+                );
+    }
+
+    @ExceptionHandler(MessagingException.class)
+    public ResponseEntity<ExceptionResponse> handleException(MessagingException e) {
+        return ResponseEntity
+                .status(INTERNAL_SERVER_ERROR)
+                .body(ExceptionResponse.builder()
+                        .error(e.getMessage())
+                        .build()
+                );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleException(MethodArgumentNotValidException e) {
+        Set<String> errors = new HashSet<>();
+        e.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var errorMessage = error.getDefaultMessage();
+                    errors.add(errorMessage);
+                });
+        return ResponseEntity
+                .status(BAD_REQUEST)
+                .body(ExceptionResponse.builder()
+                        .validationErrors(errors)
+                        .build());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(Exception e) {
+        // log the exception
+        e.printStackTrace();
+        return ResponseEntity
+                .status(INTERNAL_SERVER_ERROR)
+                .body(ExceptionResponse.builder()
+                        .businessExceptionDescription("Internal server error, contact the admin")
                         .error(e.getMessage())
                         .build()
                 );
